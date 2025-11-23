b54f8662af0d64f89b7d6607efb9866cc5d9d074
yuniii23 <u20191e810@upc.edu.pe>
2025-11-22 20:35:27 -0500

    Actualizacion

--- LeadYourWay-Frontend/src/app/components/bicicletacomponent/bicicletacomponent.ts
+++ LeadYourWay-Frontend/src/app/components/bicicletacomponent/bicicletacomponent.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { Bicicletalistar } from "./bicicletalistar/bicicletalistar";
 
 @Component({
   selector: 'app-bicicletacomponent',
-  imports: [],
+  imports: [RouterOutlet, Bicicletalistar],
   templateUrl: './bicicletacomponent.html',
   styleUrl: './bicicletacomponent.css',
 })
 export class Bicicletacomponent {
-
+  constructor(public route:ActivatedRoute){}
 }
--- LeadYourWay-Frontend/src/app/components/bicicletacomponent/bicicletalistar/bicicletalistar.ts
+++ LeadYourWay-Frontend/src/app/components/bicicletacomponent/bicicletalistar/bicicletalistar.ts
@@ -1,11 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Bicicletaservice } from '../../../services/bicicletaservice';
+import { Router, RouterLink } from '@angular/router';
+import { Bicicleta } from '../../../models/bicicleta';
+
+type UserRole = 'cliente' | 'suministrador' | 'invitado';
 
 @Component({
   selector: 'app-bicicletalistar',
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './bicicletalistar.html',
   styleUrl: './bicicletalistar.css',
 })
-export class Bicicletalistar {
+export class Bicicletalistar implements OnInit {
+  bicicletas: Bicicleta[] = []; // Usar minúsculas para el rol para que coincida con la condición en el HTML
+  currentUserRole: UserRole = 'suministrador';
 
+  constructor(private bS: Bicicletaservice, private router: Router) {} // Inyectar Router
+
+  ngOnInit(): void {
+  this.bS.list().subscribe((data) => {
+    this.bicicletas = data;
+    this.bS.setList(data);
+  });
+
+  this.bS.getList().subscribe((data) => {
+    this.bicicletas = data;
+  });
+}
+
+
+  listBicicletas(): void {
+    this.bS.list().subscribe((data) => {
+      this.bicicletas = data;
+      this.bS.setList(data);
+    });
+  }
+
+  onEdit(bicicleta: Bicicleta): void {
+    console.log('Navegando a edición de:', bicicleta.idBicicleta); 
+    this.router.navigate(['/bicicletas/editar', bicicleta.idBicicleta]);
+  }
+
+  onDelete(bicicleta: Bicicleta): void {
+    if (
+      confirm(
+        `¿Estás seguro de que quieres eliminar la bicicleta modelo ${bicicleta.modeloBicicleta}?`
+      )
+    ) {
+
+      this.eliminar(bicicleta.idBicicleta);
+    }
+  }
+
+  eliminar(id: number): void {
+    this.bS.delete(id).subscribe({
+      next: () => {
+        // No se necesita el 'data' del 'next' ya que el backend retorna 'text'
+        console.log(`Bicicleta ${id} eliminada.`);
+        this.bS.list().subscribe((data) => {
+          this.bS.setList(data);
+        });
+      },
+      error: (err) => {
+        console.error('Error al eliminar:', err);
+      },
+    });
+  }
 }
